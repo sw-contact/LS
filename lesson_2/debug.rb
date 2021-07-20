fee84cf7c5fd4f6062bbd01f520e1f4ef8fa7513
sw-contact <dgbsteve@gmail.com>
2021-07-20 11:47:52 +0000

    2nd commit

---
+++ lesson_2/debug.rb
@@ -0,0 +1,13 @@
+def car(new_car)
+  make = make(new_car)
+  model = model(new_car)
+  [make, model]
+end
+
+def make(new_car)
+  new_car.split(" ")[0]
+end
+
+def model(new_car)
+  new_car.split(" ")[1]
+end
