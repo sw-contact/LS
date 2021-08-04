5b86a376136bd725854083612d84a70c9fa6369e
sw-contact <dgbsteve@gmail.com>
2021-08-04 23:33:13 +0000

    8/4/21 commit

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,36 +1,85 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = { 'r' => 'rock',
+                  'p' => 'paper',
+                  'sc' => 'scissors',
+                  'l' => 'lizard',
+                  'sp' => 'spock' }
+
+WINNING = {
+  'r' => { wins: ['sc', 'l'] },
+  'p' => { wins: ['r', 'sp'] },
+  'sc' => { wins: ['p', 'l'] },
+  'l' => { wins: ['p', 'sp'] },
+  'sp' => { wins: ['r', 'sc'] }
+}
+
+GAME_SCORE = { player: 0, computer: 0 }
 
 def prompt(message)
-  puts("-> #{message}")
+  puts("===> #{message}")
 end
 
-loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
+def new_line
+  puts "\n"
+end
 
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice")
-    end
-  end
+def valid_round(num)
+  num.to_i != 0 && num.to_i > 0
+end
 
-  computer_choice = VALID_CHOICES.sample
+def win?(first, second)
+  WINNING[first][:wins].include?(second)
+end
 
-  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
-
-  if (choice == 'rock' && computer_choice == 'scissors') ||
-     (choice == 'paper' && computer_choice == 'rock') ||
-     (choice == 'scissors' && computer_choice == 'paper')
+def display_results(player, computer)
+  if win?(player, computer)
     prompt("You won!")
-  elsif (choice == 'rock' && computer_choice == 'paper') ||
-        (choice == 'paper' && computer_choice == 'scissors') ||
-        (choice == 'scissors' && computer_choice == 'rock')
+  elsif win?(computer, player)
     prompt("Computer won!")
   else
     prompt("It's a tie!")
+  end
+end
+
+choice = ''
+rounds = ''
+computer_choice = ''
+count = 0
+
+prompt("Welcome to the Rock, Paper, Scissors, Lizard, Spock game!")
+
+loop do
+  prompt("How many rounds to win? (Please enter an integer)")
+  rounds = gets.chomp
+  break if valid_round(rounds.to_i)
+  prompt("That's not a valid choice, please try again")
+end
+
+loop do
+  loop do
+    loop do
+      prompt("Choose one:")
+      VALID_CHOICES.each do |key, value|
+        puts "#{key} for #{value}"
+      end
+      choice = gets.chomp
+
+      if VALID_CHOICES.include?(choice)
+        break
+      else
+        prompt("That's not a valid choice")
+      end
+    end
+
+    computer_choice = VALID_CHOICES.keys.sample
+
+    prompt("\n\t You chose: #{VALID_CHOICES[choice]}
+            \n\t Computer chose: #{VALID_CHOICES[computer_choice]}")
+    new_line
+
+    display_results(choice, computer_choice)
+
+    count += 1
+    break if
   end
 
   prompt("Do you want to play again?")
---
+++ lesson_3/test.rb
@@ -0,0 +1,21 @@
+def is_an_ip_number?(num)
+  (0..255).include?(num.to_i)
+end
+
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false unless dot_separated_words.size == 4
+
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false unless is_an_ip_number?(word)
+  end
+
+  true
+end
+
+def correct_ip_num?(number)
+  puts 'true' if dot_separated_ip_address?(number)
+end
+
+correct_ip_num?('10.4.5')
